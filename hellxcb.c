da832a8a271f1ef66384a678df0eec6414d6bb60
su8 <wifiextender2@gmail.com>
2024-08-10 10:56:50 +0300

    show the opened windows to the 'grid' function too

--- hellxcb.c
+++ hellxcb.c
@@ -612,6 +612,7 @@ void grabkeys(void) {
 
 /* arrange windows in a grid */
 void grid(int hh, int cy) {
+    numOfWindows = 0U;
     int n = 0, cols = 0, cn = 0, rn = 0, i = -1;
     for (client *c = head; c; c=c->next) if (!ISFFT(c)) ++n;
     for (cols=0; cols <= n/2; cols++) if (cols*cols >= n) break; /* emulate square root */
@@ -619,7 +620,7 @@ void grid(int hh, int cy) {
 
     int rows = n/cols, ch = hh - BORDER_WIDTH, cw = (ww - BORDER_WIDTH)/(cols?cols:1);
     for (client *c=head; c; c=c->next) {
-        if (ISFFT(c)) continue; else ++i;
+        if (ISFFT(c)) continue; else { ++i; numOfWindows++; }
         if (i/rows + 1 > cols - n%cols) rows = n/cols + 1;
         xcb_move_resize(dis, c->win, cn*cw, cy + rn*ch/rows, cw - BORDER_WIDTH, ch/rows - BORDER_WIDTH);
         if (++rn >= rows) { rn = 0; cn++; }
