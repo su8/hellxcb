7278da4e0cb981e1fe0506056ff8ecc57748fdaf
su8 <wifiextender2@gmail.com>
2025-03-02 06:19:35 +0200

    Update config.h to use border width of 2 pixels

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ MA 02110-1301, USA.
 #define FOLLOW_MOUSE    True      /* Focus the window the mouse just entered */
 #define FOLLOW_WINDOW   False     /* Follow the window when moved to a different desktop */
 #define CLICK_TO_FOCUS  False     /* Focus an unfocused window when clicked */
-#define BORDER_WIDTH    5         /* window border width */
+#define BORDER_WIDTH    2         /* window border width */
 #define UNFOCUS         "#444444" /* unfocused window border color */
 #define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
 #define DEFAULT_DESKTOP 0         /* the desktop to focus on exec */
